48aa39980fdb827733fffad10a85afde97a539f9
Guilherme Laureano <39038762+guilhermelaureano@users.noreply.github.com>
2023-08-11 03:21:52 +0000

    add first page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home () {
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <h1>Content</h1>
+            <h1>footer</h1>
+        </div>
+    );
+
+}
+
+export default Home;
