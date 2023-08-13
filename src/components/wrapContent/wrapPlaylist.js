08a439a9ea40d38e1e6e485ba84c4afcc449a755
Guilherme Laureano <guilhermeml001@gmail.com>
2023-08-13 19:18:35 -0300

    refactor playlist welcome message

--- src/components/wrapContent/wrapPlaylist.js
+++ src/components/wrapContent/wrapPlaylist.js
@@ -8,8 +8,8 @@ function WrapPlaylist() {
   if (!data?.items && !loadingSearch) {
     return (
       <div className="max-w-5xl mx-auto p-4 md:p-12 flex items-center justify-center">
-        <div className="px-24 py-16 border-none rounded-2xl bg-gradient-to-br from-pink-500 to-orange-400">
-          <p className="font-semibold text-lg md:text-2xl lg:text-3xl text-slate-800 dark:text-slate-100">
+        <div className="px-20 py-8 border-none rounded-2xl bg-gradient-to-br from-pink-500 to-orange-400">
+          <p className="font-semibold text-lg md:text-2xl lg:text-3xl text-slate-100 ">
             O que você gostaria de ver no YouTube?
           </p>
         </div>
