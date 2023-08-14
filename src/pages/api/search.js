008aa8cf79610e39b62e1340b033dfe0cc416312
Guilherme Laureano <guilhermeml001@gmail.com>
2023-08-14 20:00:11 -0300

    refactor: get search

---
+++ src/pages/api/search.js
@@ -0,0 +1,24 @@
+async function getSearch(term, page = '') {
+  const url = new URL(process.env.NEXT_PUBLIC_URL_YOUTUBE_API_SEARCH);
+  const params = {
+    key: process.env.NEXT_PUBLIC_API_KEY,
+    maxResults: 50,
+    order: 'relevance',
+    part: 'snippet',
+    q: term,
+    type: 'video',
+    pageToken: page,
+  };
+  params.Object.keys(params).forEach(key =>
+    url.searchParams.append(key, params[key]),
+  );
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export { getSearch };
