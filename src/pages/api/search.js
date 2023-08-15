a5cef458b4b13b9203c1e3d1138218dffebdabe4
Guilherme Laureano <guilhermeml001@gmail.com>
2023-08-15 20:43:27 -0300

    fix search params

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -2,16 +2,18 @@ import axios from 'axios';
 
 async function getSearch(term, page = '') {
   const url = process.env.NEXT_PUBLIC_URL_YOUTUBE_API_SEARCH;
+  const params = {
+    key: process.env.NEXT_PUBLIC_API_KEY,
+    maxResults: 50,
+    order: 'relevance',
+    part: 'snippet',
+    q: term,
+    type: 'video',
+    pageToken: page,
+  };
+
   try {
-    const { data } = await axios.get(url, {
-      key: process.env.NEXT_PUBLIC_API_KEY,
-      maxResults: 50,
-      order: 'relevance',
-      part: 'snippet',
-      q: term,
-      type: 'video',
-      pageToken: page,
-    });
+    const { data } = await axios.get(url, { params });
     return data;
   } catch (e) {
     throw new Error('Failed to fetch data getSearch', e);
