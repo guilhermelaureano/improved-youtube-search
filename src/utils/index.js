4f91a09c8c93a17e10d32b08acc49de08b838c40
Guilherme Laureano <guilhermeml001@gmail.com>
2023-08-14 22:38:33 -0300

    feat: (format) items and time

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export function getParameter(items) {
   const desc = [];
   const id = [];
@@ -15,22 +17,47 @@ export function getParameter(items) {
 export function concatParameter(prev, result) {
   const { desc, id, title } = getParameter(result.items);
 
-  const data = prev.data.concat(result.items);
+  const resultData = prev.itemsList.concat(result.items);
   const descWords = prev.descWords.concat(desc);
   const idList = prev.idList.concat(id);
   const titleWords = prev.titleWords.concat(title);
-
-  const pageToken = result.nextPageToken && '';
   const totalItems = prev.totalItems + result.pageInfo.resultsPerPage;
-  const totalResults = result.pageInfo.totalResults;
 
   return {
-    data,
     descWords,
     idList,
-    pageToken,
+    resultData,
     titleWords,
     totalItems,
-    totalResults,
   };
+}
+
+export function formatItems(itemsList, contentDetailsList) {
+  const newItemsList = itemsList.map(item => {
+    const newItem = {
+      channelId: item.snippet.channelId,
+      channelTitle: item.snippet.channelTitle,
+      description: item.snippet.description,
+      id: item.id.videoId,
+      title: item.snippet.title,
+      imgURL: item.snippet.thumbnails.medium.url,
+      imgURL: item.snippet.thumbnails.medium.url,
+    };
+
+    const content = contentDetailsList.find(
+      content => content.id === item.id.videoId,
+    );
+    newItem.duration = content ? content.contentDetails?.duration : '';
+
+    return newItem;
+  });
+
+  return newItemsList;
+}
+
+export function formatTime(duration) {
+  const secondsAmount = moment.duration(duration).asSeconds();
+  const minutes = Math.floor(secondsAmount / 60);
+  const seconds = String(secondsAmount % 60).padStart(2, '0');
+  return `${minutes}:${seconds}`;
 }
